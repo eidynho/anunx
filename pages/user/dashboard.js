085608b412c52bb867008e8124ea342b987e9b7c
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-04-01 15:00:25 -0300

    adicionando template default, Header, User dashboard

---
+++ pages/user/dashboard.js
@@ -0,0 +1,118 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material'
+
+import TemplateDefault from '../../src/templates/Default'
+
+export default function Home() {
+  
+  return (
+    <TemplateDefault>
+      <Container maxWidth="sm" sx={{ padding: (8, 0, 6), }}>
+        <Typography component="h1" variant="h2" align="center">
+          Meus anúncios
+        </Typography>
+        <Button variant="contained" color="primary" sx={{ margin: '30px auto', display: 'block' }}>
+          Publicar novo anúncio
+        </Button>
+      </Container>
+      
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+                sx={{
+                  paddingTop: '56%'
+                }}
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+                sx={{
+                  paddingTop: '56%'
+                }}
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+                sx={{
+                  paddingTop: '56%'
+                }}
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+        </Grid>
+      </Container>
+    </TemplateDefault>
+  )
+}
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    }
+  }
+})
+
+export default theme
