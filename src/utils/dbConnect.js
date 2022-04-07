3a349ac26e4d118a6c3d9dc469c6432440d30247
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-04-06 21:19:31 -0300

    adding cjs-loader

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -8,18 +8,13 @@ if (!MONGODB_URI) {
   )
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
 let cached = global.mongoose
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null }
 }
 
-async function dbConnect() {
+const dbConnect = async () => {
   if (cached.conn) {
     return cached.conn
   }
